f02903c2db43f1a75aa3a8abbabacf78ca22f329
experionarjun <arjun.sreedhar@experionglobal.com>
2017-03-14 19:01:09 +0530

    Native API

--- src/pages/home/views/details/details.ts
+++ src/pages/home/views/details/details.ts
@@ -1,9 +1,18 @@
 import { Component, ViewChild, ElementRef  } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-import { Geolocation,
-          
-        } from 'ionic-native';
+// import { Geolocation} from 'ionic-native';
+import {
+  Geolocation,
+ GoogleMap,
+ GoogleMapsEvent,
+ GoogleMapsLatLng,
+ CameraPosition,
+ GoogleMapsMarkerOptions,
+ GoogleMapsMarker,
+ GoogleMapsMapTypeId
+} from 'ionic-native';
+
 declare var google;
 /*
   Generated class for the Details page.
@@ -18,8 +27,9 @@ declare var google;
 })
 export class DetailsPage {
   
-  @ViewChild('map') mapElement: ElementRef;
-  map: any;
+  // @ViewChild('map') mapElement: ElementRef;
+  // map: any;
+
   // post:any;
   // lat:any;
   // long:any;
@@ -47,54 +57,91 @@ export class DetailsPage {
    
   // }
  
-  loadMap(){
+  // loadMap(){
  
-   Geolocation.getCurrentPosition().then((position) => {
+  //  Geolocation.getCurrentPosition().then((position) => {
  
-      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  //     let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
  
-      let mapOptions = {
-        center: latLng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
+  //     let mapOptions = {
+  //       center: latLng,
+  //       zoom: 15,
+  //       mapTypeId: google.maps.MapTypeId.ROADMAP
+  //     }
  
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  //     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
  
-    }, (err) => {
-      console.log(err);
-    });
+  //   }, (err) => {
+  //     console.log(err);
+  //   });
  
-  }
+  // }
  
-addMarker(){
+// addMarker(){
  
-  let marker = new google.maps.Marker({
-    map: this.map,
-    animation: google.maps.Animation.DROP,
-    position: this.map.getCenter()
-  });
+//   let marker = new google.maps.Marker({
+//     map: this.map,
+//     animation: google.maps.Animation.DROP,
+//     position: this.map.getCenter()
+//   });
  
-  let content = "<h4>Information!</h4>";          
+//   let content = "<h4>Information!</h4>";          
  
-  this.addInfoWindow(marker, content);
+//   this.addInfoWindow(marker, content);
  
-}
+// }
 
-addInfoWindow(marker, content){
+// addInfoWindow(marker, content){
  
-  let infoWindow = new google.maps.InfoWindow({
-    content: content
-  });
+//   let infoWindow = new google.maps.InfoWindow({
+//     content: content
+//   });
  
-  google.maps.event.addListener(marker, 'click', () => {
-    infoWindow.open(this.map, marker);
-  });
+//   google.maps.event.addListener(marker, 'click', () => {
+//     infoWindow.open(this.map, marker);
+//   });
  
-}
+// }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DetailsPage');
   }
 
+loadMap() {
+ // make sure to create following structure in your view.html file
+ // and add a height (for example 100%) to it, else the map won't be visible
+ // <ion-content>
+ //  <div #map id="map" style="height:100%;"></div>
+ // </ion-content>
+ // create a new map by passing HTMLElement
+ let element: HTMLElement = document.getElementById('map');
+ let map = new GoogleMap(element);
+ // create LatLng object
+ let ionic: GoogleMapsLatLng = new GoogleMapsLatLng(43.0741904,-89.3809802);
+ // create CameraPosition
+ let position: CameraPosition = {
+   target: ionic,
+   zoom: 18,
+   tilt: 30
+ };
+ // listen to MAP_READY event
+ map.one(GoogleMapsEvent.MAP_READY).then(() => {
+   // move the map's camera to position
+   map.moveCamera(position); // works on iOS and Android
+});
+ // create new marker
+ let markerOptions: GoogleMapsMarkerOptions = {
+   position: ionic,
+   title: 'Ionic'
+ };
+ map.addMarker(markerOptions)
+   .then((marker: GoogleMapsMarker) => {
+      marker.showInfoWindow();
+    });
+ }
+
+
+
 }
+
+
